0937d15cb3cee91390392e434a6e250c9c656f9f
Gronunder <120636831+Gronunder@users.noreply.github.com>
2024-09-30 17:29:33 +0300

    Update WeatherApp1.0.ps1
    Spelling error update on comments

--- WeatherApp1.0.ps1
+++ WeatherApp1.0.ps1
@@ -1,23 +1,23 @@
 # Fetch the API key from an external file
-$apiKeyFilePath = "C:\WeatherAPIkey.txt"  # <- When using FilePath, please update your path if not in default location. !!! Haven't tested this with any Secrets Manager/Server !!!
+$apiKeyFilePath = "C:\WeatherAPIkey.txt"  # <- When using FilePath, please update your path if not in the default location. !!! I haven't tested this with any Secrets Manager/Server !!!
 if (-not (Test-Path $apiKeyFilePath)) {
     Write-Error "API key file not found at $apiKeyFilePath." -ForegroundColor Black -BackgroundColor Red  #Graceful Error
     exit
 }
 $apiKey = Get-Content -Path $apiKeyFilePath -ErrorAction Stop
 
-# Checkout step for APi validity. This step will shout out if there is an error/missing Key or unreconized file. 
+# Checkout step for APi validity. This step will shout out if there is an error/missing Key or unrecognized file. 
 if (-not $apiKey) {
     Write-Error "API key is empty or invalid."  -ForegroundColor Black -BackgroundColor Red
     exit
 }
 
-# Write-Host will promopt the user select a city. Engligh and Cirillic is tested only! 
-# Nice to have: Basic list of cities in EU promp. 
+# Write-Host will prompt the user to select a city. English and Cyrillic is tested only! 
+# Nice to have: Basic list of cities in EU prompt. 
 Write-Host "Please enter the name of the city (Cyrillic or English entries tested):" -ForegroundColor Black -BackgroundColor Green
 $city = Read-Host
 
-# Main Function/Process to fetch and display the "5-day weather forecast" - I've done a 5 day due to a limimation on the API request of up-to 5-day forecast for free accounts. 
+# Main Function/Process to fetch and display the "5-day weather forecast" - I've done a 5-day due to a limitation on the API request of up-to 5-day forecast for free accounts. 
 function Get-5DayForecast {
     param (
         [string]$city,
@@ -36,9 +36,9 @@ function Get-5DayForecast {
         if ($response -and $response.list) {
             $forecastData = $response.list | Group-Object { $_.dt_txt.Split(" ")[0] } | Select-Object -First 5
 
-            # Create a table to display the forecast information - Table created using official documenation
+            # Create a table to display the forecast information - Table created using official documentation
             $table = @()
-            foreach ($group in $forecastData) {  # This part should sort out the day, remove the time as it has 3h ticks only. Resoruces: Forums and Youtube. 
+            foreach ($group in $forecastData) {  # This part should sort out the day, remove the time as it has 3h ticks only. Resources: Forums and YouTube. 
                 $day = $group.Group[0]  # Get the first entry for the day
                 $date = $group.Name     # Grouped by date and removed the time, due to the 3h ticks available for this API key. 
                 $tempMin = [math]::Round($day.main.temp_min)
@@ -56,18 +56,18 @@ function Get-5DayForecast {
                 }
             }
 
-            # This is the table which is set with PS default -AutoSize. Need to investigate more time for other options.. 
+            # This is the table that is set with PS default -AutoSize. Need to investigate other format-table options... 
             $table | Format-Table -AutoSize
         }
     }
     catch {
-        # Error/Issue handling. These are basic error messages that will be outputed at the same time, due the unified block. Idea is to navigate the end user to better troubleshoot the issue. Note: I managed to generate "cod":404 error only, as I think this the only one for the "City" API's. 
+        # Error/Issue handling. These are basic error messages that will be outputted at the same time, due to the unified block. The idea is to navigate the end user to better troubleshoot the issue. Note: I managed to generate "cod":404 error only, as I think this the only one for the "City" API's. 
         Write-Host "An error occurred while trying to fetch the weather data for ${city}." -ForegroundColor Black -BackgroundColor Red
         Write-Host "API error details, or Error code: $_" -ForegroundColor Red 
     }
 }
 
-# Call the function that is started on line"5DayForecast. I've used this name, as it was a defaul one in the OpenWeatherMap Documenation! 
+# Call the function that is started on line"5DayForecast. I've used this name, as it was a default one in the OpenWeatherMap documentation! 
 Get-5DayForecast -city $city -apiKey $apiKey
 
 
